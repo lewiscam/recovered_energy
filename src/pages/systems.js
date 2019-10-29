e4087f24f39683baa12d962f782ae7f0887b2066
Tyler <tyler@recoveredenergy.com>
2019-10-29 15:11:38 -0600

    prettier formatting

--- src/pages/systems.js
+++ src/pages/systems.js
@@ -3,39 +3,31 @@ import { Link } from "gatsby"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Container, Image, Grid} from "semantic-ui-react"
-const Systems =  ({ data: { allPrismicModel } }) => {
+import { Container, Image, Grid } from "semantic-ui-react"
+const Systems = ({ data: { allPrismicModel } }) => {
   const allProducts = allPrismicModel.edges
 
+  const displayProduct = allProducts.map(product => {
+    return (
+      <Grid.Column className="main-container">
+        <Link to={"/products/" + product.node.uid} key={product.node.uid}>
+          <h2>{product.node.data.product_name.text}</h2>
+          <Image src={product.node.data.model_feature_image.url} />
+        </Link>
+      </Grid.Column>
+    )
+  })
 
-const displayProduct =
- allProducts.map(product => {
-     return (
-       <Grid.Column className="main-container">
-         <Link to={"/products/" + product.node.uid} key={product.node.uid}>
-           <h2>{product.node.data.product_name.text}</h2>
-           <Image src={product.node.data.model_feature_image.url} />
-         </Link>
-       </Grid.Column>
-     )
- });
-
- ////// not using yet. still working on getting this to work ///////
-// const currentSystemCategory =
-// allProducts.filter(currentCategory =>
-//     currentCategory.node.data.system_category.document[0].uid === window.history.state.item.uid
-//     )
-
-
-
-
+  ////// not using yet. still working on getting this to work ///////
+  // const currentSystemCategory =
+  // allProducts.filter(currentCategory =>
+  //     currentCategory.node.data.system_category.document[0].uid === window.history.state.item.uid
+  //     )
 
   return (
     <Layout>
       <Grid>
-        <Grid.Row columns={4}>
-      {displayProduct}
-        </Grid.Row>
+        <Grid.Row columns={4}>{displayProduct}</Grid.Row>
       </Grid>
     </Layout>
   )
@@ -44,33 +36,30 @@ const displayProduct =
 export default Systems
 
 export const systemsQuery = graphql`
-         query getProducts {
-           allPrismicModel {
-             edges {
-               node {
-                 data {
-                   model_feature_image {
-                     url
-                   }
-                   product_name {
-                     text
-                   }
-                   system_category {
-                     document {
-                       data {
-                         system_category_title {
-                           text
-                         }
-                       }
-                     }
-                   }
-                 }
-                 uid
-               }
-             }
-           }
-         }
-       `
-
-
-
+  query getProducts {
+    allPrismicModel {
+      edges {
+        node {
+          data {
+            model_feature_image {
+              url
+            }
+            product_name {
+              text
+            }
+            system_category {
+              document {
+                data {
+                  system_category_title {
+                    text
+                  }
+                }
+              }
+            }
+          }
+          uid
+        }
+      }
+    }
+  }
+`
